849d4b70d251c28e57438442393571fee4b24330
1stVyacheslav <komaroff4@gmail.com>
2020-02-12 10:01:56 +0500

    refact(webpack): add ability start project on windows

--- webpack.config.js
+++ webpack.config.js
@@ -5,26 +5,26 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const autoprefixerPlugin = require('autoprefixer');
 
-const devMode = process.env.NODE_ENV === 'development';
-const pluginName = 'item-quantity-dropdown';
+module.exports = (env = {}) => {
+  const devMode = (env.mode === 'development') || (process.env.NODE_ENV === 'development');
+  const pluginName = 'item-quantity-dropdown';
 
-const config = {
-  mode: process.env.NODE_ENV,
+  const config = {
+    mode: env.mode ? env.mode : process.env.NODE_ENV,
 
-  entry: './src/index.js',
+    entry: './src/index.js',
 
-  output: {
-    path: path.resolve(__dirname, 'lib'),
-    filename: devMode ? `${pluginName}.js` : `${pluginName}.min.js`,
-    library: pluginName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true,
-  },
+    output: {
+      path: path.resolve(__dirname, 'lib'),
+      filename: devMode ? `${pluginName}.js` : `${pluginName}.min.js`,
+      library: pluginName,
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+    },
 
-  module: {
-    rules: [
-      {
-        // ESLint
+    module: {
+      rules: [{
+      // ESLint
         enforce: 'pre',
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
@@ -34,14 +34,12 @@ const config = {
         // JS
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
           },
-        ],
+        }],
       },
       {
         // Expose jQuery
@@ -52,36 +50,35 @@ const config = {
       {
         // CSS / SASS
         test: /\.s?css$/,
-        use: [
-          {
-            loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+        use: [{
+          loader: devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+        },
+        {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
           },
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
+        },
+        {
+          loader: 'postcss-loader',
+          options: {
+            plugins: [autoprefixerPlugin],
+            sourceMap: true,
           },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [autoprefixerPlugin],
-              sourceMap: true,
-            },
+        },
+        {
+          loader: 'resolve-url-loader',
+          options: {
+            sourceMap: true,
           },
-          {
-            loader: 'resolve-url-loader',
-            options: {
-              sourceMap: true,
-            },
+        },
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+            includePaths: [path.resolve(__dirname, 'src')],
           },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-              includePaths: [path.resolve(__dirname, 'src')],
-            },
-          },
+        },
         ],
       },
       {
@@ -89,34 +86,37 @@ const config = {
         test: /\.(jpe?g|png|gif|svg)$/i,
         use: 'url-loader',
       },
+      ],
+    },
+
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: `${pluginName}.min.css`,
+      }),
+      new StyleLintPlugin({
+        configFile: path.resolve(__dirname, '.stylelintrc.json'),
+        context: path.resolve(__dirname, 'src'),
+      }),
     ],
-  },
 
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: `${pluginName}.min.css`,
-    }),
-    new StyleLintPlugin({
-      configFile: path.resolve(__dirname, '.stylelintrc.json'),
-      context: path.resolve(__dirname, 'src'),
-    }),
-  ],
+    resolve: {
+      extensions: ['.js', '.css', '.scss'],
+      modules: ['src', 'node_modules'],
+    },
 
-  resolve: {
-    extensions: ['.js', '.css', '.scss'],
-    modules: ['src', 'node_modules'],
-  },
+    devtool: 'source-map',
+  };
 
-  devtool: 'source-map',
+  if (devMode) {
+    config.plugins.push(
+      new HtmlWebpackPlugin({
+        template: 'test/index.html',
+      }),
+    );
+    if (process.env.NODE_ENV === 'development') {
+      config.devServer = { host: '0.0.0.0' };
+    }
+  }
+
+  return config;
 };
-
-if (devMode) {
-  config.plugins.push(
-    new HtmlWebpackPlugin({
-      template: 'test/index.html',
-    }),
-  );
-  config.devServer = { host: '0.0.0.0' };
-}
-
-module.exports = config;
